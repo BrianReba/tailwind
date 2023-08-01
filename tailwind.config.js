7228c63200e1cd91a1a4042b0278b400c5a5a49b
BrianReba <brianezequiel.reba@gmail.com>
2023-08-01 17:42:03 -0300

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./build/*.html'],
+  content: ['./build/*.html','./build/js/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
+        'tallscren': { 'raw': '(min-aspect-ratio: 13/20)'}
+      },
+      keyframe: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)'},
+          '80%': { transform: 'scaleY(1.2)'},
+          '100%': { transform: 'scaleY(1)'},
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      }
+    },
   },
   plugins: [],
 }
